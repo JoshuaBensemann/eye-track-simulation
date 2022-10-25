c1a6cde974f73ed39c7c2c659931882018a03d75
Joshua Bensemann <joshuabensemann@gmail.com>
2022-10-25 21:30:18 +1300

    added automatic text to csv loop for models

--- synthetic_df_creation.py
+++ synthetic_df_creation.py
@@ -14,8 +14,13 @@ def main():
         print(models)
 
     for text in text_names:
-        with open(f"{input_dir}{text}.txt", "r") as f:
-            text_data = f.readlines()
+
+        try:
+            with open(f"{input_dir}{text}.txt", "r") as f:
+                text_data = f.readlines()
+        except UnicodeDecodeError:
+            with open(f"{input_dir}{text}.txt", "r", encoding="latin-1") as f:
+                text_data = f.readlines()
 
         for model in models:
             filename = f"{output_dir}{text}_{model}.csv"
